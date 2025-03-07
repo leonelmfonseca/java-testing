d766b6e869b654e12baa6affc5b97868aeda13c2
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-07 13:14:49 +0000

    test: add test class

---
+++ src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccountTest.java
@@ -0,0 +1,10 @@
+package com.developer.simpledemo.javatesting.jupiterannotations.bankaccount;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class BankAccountTest {
+
+
+}
