379e82a81b914dd2882ac60b0a8ab9c2bde3e7ba
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-07 12:36:39 +0000

    feat: initial commit

---
+++ src/main/java/com/developer/simpledemo/javatesting/jupiterannotations/JunitJupiterAnnotationsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.developer.simpledemo.javatesting.jupiterannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitJupiterAnnotationsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitJupiterAnnotationsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/JunitJupiterAnnotationsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developer.simpledemo.javatesting.jupiterannotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitJupiterAnnotationsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
