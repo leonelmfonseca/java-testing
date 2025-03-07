71bc0e1588ce81d7cb1751fe2a96178fcb4feb64
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-07 12:59:58 +0000

    feat: add test target class

--- src/main/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccount.java
+++ src/main/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccount.java
@@ -1,3 +1,40 @@
 package com.developer.simpledemo.javatesting.jupiterannotations.bankaccount;
 
-public class BankAccount {}
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@NoArgsConstructor
+@Getter
+@ToString
+public class BankAccount {
+  private double balance;
+
+  public BankAccount(double initialBalance) {
+    if (initialBalance < 0) {
+      throw new IllegalArgumentException("Initial balance cannot be negative.");
+    }
+    this.balance = initialBalance;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public void deposit(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Deposit amount must be positive.");
+    }
+    balance += amount;
+  }
+
+  public void withdraw(double amount) {
+    if (amount <= 0) {
+      throw new IllegalArgumentException("Withdrawal amount must be positive.");
+    }
+    if (amount > balance) {
+      throw new IllegalArgumentException("Insufficient balance.");
+    }
+    balance -= amount;
+  }
+}
