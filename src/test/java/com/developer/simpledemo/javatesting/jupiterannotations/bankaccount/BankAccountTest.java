5b93c4af9edf0b90c9a450a47907cb8293504f6c
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-07 13:15:47 +0000

    feat(test): @Test demo

--- src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccountTest.java
+++ src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccountTest.java
@@ -6,5 +6,15 @@ import org.junit.jupiter.api.Test;
 
 class BankAccountTest {
 
+  private static final double INITIAL_BALANCE = 1000D;
 
+  private final BankAccount bankAccount = new BankAccount(INITIAL_BALANCE);
+
+  
+  @Test // Denotes that a method is a test method.
+  void depositTest() {
+    double depositAmount = 10D;
+    bankAccount.deposit(depositAmount);
+    assertEquals(INITIAL_BALANCE + depositAmount, bankAccount.getBalance());
+  }
 }
