cc6c38c086d06a726a233dc48af1423a7d07ef60
leonelmfonseca <leonelfonseca@gmail.com>
2025-03-10 17:53:36 +0000

    test: add @Nested tests and implement @BeforeAll, @BeforeEach, @AfterAll and @AfterEach

--- src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccountImplTest.java
+++ src/test/java/com/developer/simpledemo/javatesting/jupiterannotations/bankaccount/BankAccountImplTest.java
@@ -15,11 +15,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class BankAccountImplTest {
 
@@ -27,29 +30,85 @@ class BankAccountImplTest {
 
   private BankAccount bankAccountImpl;
   private final Faker faker = new Faker();
-  
-  @BeforeEach
-  void init() {
-    System.out.println("Setting up new BankAccount instance...");
-    bankAccountImpl = new BankAccountImpl(INITIAL_BALANCE); // Initialize before each test
-  }
+  private static Logger logger;
 
+  // Initialize ONCE before all test
   @BeforeAll
   static void initAll() {
     System.out.println("Starting tests for BankAccountImpl...");
     // Example: Could be initializing a mock database or other shared resources
+    logger = LoggerFactory.getLogger(BankAccountImplTest.class);
   }
 
-  @DisplayName("Test demo")
+  @AfterAll
+  static void tearDownAll() {
+    System.out.println("All tests completed. Cleaning up resources...");
+    // Example: Could be closing a database connection or clearing caches
+  }
+
+  @BeforeEach
+  void init() {
+    System.out.println("Setting up new BankAccount instance...");
+    // Initialize before EACH test
+    bankAccountImpl = new BankAccountImpl(INITIAL_BALANCE);
+    logger.info("Balance: {}", bankAccountImpl.getBalance());
+  }
+
+  @AfterEach
+  void tearDown() {
+    System.out.println("Cleaning up after test...");
+    bankAccountImpl = null; // Clear the instance to ensure no shared state
+  }
+
+  @DisplayName("Test withdraw amount")
   // Denotes that a method is a test method.
   @Test
-  void updatedBalance_EqualToSumOfDepositWithInitialBalance() {
-    double depositAmount = 10D;
-    bankAccountImpl.deposit(depositAmount);
-    assertEquals(INITIAL_BALANCE + depositAmount, bankAccountImpl.getBalance());
+  void testWithdrawAmount() {
+    double withdrawAmount = generatePositiveRandomAmount((int) INITIAL_BALANCE);
+    logger.info("Withdraw amount: {}", withdrawAmount);
+    bankAccountImpl.withdraw(withdrawAmount);
+    logger.info("Account balance: {}", bankAccountImpl.getBalance());
+    assertEquals(INITIAL_BALANCE - withdrawAmount, bankAccountImpl.getBalance());
+  }
+  
+  private Double generatePositiveRandomAmount(int max) {
+    return ThreadLocalRandom.current().nextDouble(0, max);
+  }
+  
+  // Used when you want to organize test cases that share a common context
+  @Nested
+  @DisplayName("Valid basic operations")
+  class ValidOperations {
+
+    @DisplayName("Test deposit amount")
+    // Denotes that a method is a test method.
+    @Test
+    void testDepositAmount() {
+      // arrange
+      double depositAmount = 10D;
+      // act
+      bankAccountImpl.deposit(depositAmount);
+      logger.info("Deposited amount: {}", depositAmount);
+      logger.info("Account balance: {}", bankAccountImpl.getBalance());
+
+      // assert
+      assertEquals(INITIAL_BALANCE + depositAmount, bankAccountImpl.getBalance());
+    }
+
+    @DisplayName("Test withdraw amount")
+    // Denotes that a method is a test method.
+    @Test
+    void testWithdrawAmount() {
+      double withdrawAmount = generatePositiveRandomAmount((int) INITIAL_BALANCE);
+      logger.info("Withdraw amount: {}", withdrawAmount);
+      bankAccountImpl.withdraw(withdrawAmount);
+      logger.info("Account balance: {}", bankAccountImpl.getBalance());
+      assertEquals(INITIAL_BALANCE - withdrawAmount, bankAccountImpl.getBalance());
+    }
   }
 
   // TODO: Test renaming
+
   @DisplayName("Parameterized Test demo")
   // Parameterized tests make it possible to run a test multiple times with different arguments.
   @ParameterizedTest
@@ -67,8 +126,9 @@ class BankAccountImplTest {
   }
 
   // Repeat a test a specified number of times.
+
   // Each invocation of a repeated test behaves like the execution of a regular @Test method
-  
+
   @DisplayName("Repeated Test demo")
   @RepeatedTest(10)
   void testWithdrawal() {
@@ -122,24 +182,63 @@ class BankAccountImplTest {
                     }));
   }
   
-  @AfterEach
-  void tearDown() {
-    System.out.println("Cleaning up after test...");
-    bankAccountImpl = null; // Clear the instance to ensure no shared state
-  }
-  
-  @AfterAll
-  static void tearDownAll() {
-    System.out.println("All tests completed. Cleaning up resources...");
-    // Example: Could be closing a database connection or clearing caches
-  }
-  
 
   private List<Double> generateRandomAmounts(int size, int min, int max) {
     return IntStream.range(0, size)
         .mapToObj(i -> ThreadLocalRandom.current().nextDouble(min, max))
         .toList();
   }
+
+  @Nested
+  @DisplayName("Invalid basic operations")
+  class InvalidOperations {
+    private static Logger loggerInvalidOps;
+    private BankAccount bankAccountInvalidOps;
+
+    // Initialize ONCE before all test
+    @BeforeAll
+    static void initAll() {
+      System.out.println("Starting tests for BankAccountImpl...");
+      // Example: Could be initializing a mock database or other shared resources
+      loggerInvalidOps = LoggerFactory.getLogger(BankAccountImplTest.class);
+    }
+    
+    @BeforeEach
+    void init() {
+      System.out.println("Setting up new BankAccount instance...");
+      // Initialize before EACH test
+      bankAccountInvalidOps = new BankAccountImpl(INITIAL_BALANCE);
+      loggerInvalidOps.info("Balance: {}", bankAccountInvalidOps.getBalance());
+    }
+    
+    @AfterEach
+    void tearDown() {
+      System.out.println("Cleaning up after test...");
+      bankAccountInvalidOps = null; // Clear the instance to ensure no shared state
+    }
+    
+    @DisplayName("Zeroing account")
+    @Test
+    void testZeroingAccountByMultipleWithdraws() {
+
+      
+      
+      assertThrows(
+          IllegalArgumentException.class,
+          () -> {
+            while (bankAccountInvalidOps.getBalance() > 0) {
+
+              Double withdraw = generatePositiveRandomAmount(144);
+              loggerInvalidOps.info("Withdraw: {}", withdraw);
+              bankAccountInvalidOps.withdraw(withdraw);
+              loggerInvalidOps.info("Balance: {}", bankAccountInvalidOps.getBalance());
+            }
+          },
+          "Negative deposit should throw exception");
+    }
+    }
+
+  
 
   // Helper method to validate initial balance or return null for invalid balance
   private BankAccountImpl createAccountOrValidateFailure(double initialBalance) {
@@ -173,4 +272,5 @@ class BankAccountImplTest {
           String.format("Expected balance mismatch for deposit %.2f", depositAmount));
     }
   }
+  
 }
